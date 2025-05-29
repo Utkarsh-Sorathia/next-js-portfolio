c427b6bbf4c4c3f276768b7b473d23a01cc7b355
Utkarsh-Sorathia <83358150+Utkarsh-Sorathia@users.noreply.github.com>
2025-05-29 05:06:28 +0000

    Updated Footer.tsx

--- src/Components/UI/Footer.tsx
+++ src/Components/UI/Footer.tsx
@@ -1,42 +1,40 @@
-// components/UI/Footer.tsx
-
-// import Link from 'next/link'
-// import { socialLinks } from '../../data/importantLinks'
+import Link from "next/link";
+import socialLinks from "../../data/importantLinks";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          {/* Logo or Title */}
-          {/* <div className="flex text-center md:text-left mb-4 md:mb-0">
-            <h2 className="text-2xl font-bold text-[#4361ee]">Utkarsh Sorathia</h2>
-            <p className="text-sm text-gray-400">Full Stack Developer | Building amazing projects</p>
-          </div> */}
-
-          {/* Social Links */}
-          {/* <div className="flex space-x-6 mb-4 md:mb-0">
-            {socialLinks.map((link, index) => (
-              <Link
-                key={index}
-                href={link.url}
-                target="_blank"
-                aria-label={link.name}
-                className="text-white text-2xl hover:text-[#4361ee] transition duration-300"
-              >
-                <i className={link.icon} />
-              </Link>
-            ))}
-          </div> */}
-        </div>
-
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row items-center md:justify-between">
         {/* Copyright */}
-        <div className="text-center text-sm text-gray-400">
-          <p>&copy; {new Date().getFullYear()} <span className="text-[var(--primaryColor)]">Utkarsh Sorathia.</span> All rights reserved.</p>
+        <div className="text-center md:text-left text-md text-gray-400 mb-4 md:mb-0">
+          <p>
+            &copy; {new Date().getFullYear()}{" "}
+            <span className="text-[var(--primaryColor)]">
+              Utkarsh Sorathia.
+            </span>{" "}
+            All rights reserved.
+          </p>
+        </div>
+        {/* Social Links */}
+        <div className="flex justify-center md:justify-end space-x-6">
+          {socialLinks.map(
+            (link, index) =>
+              index !== socialLinks.length - 1 && (
+                <Link
+                  key={index}
+                  href={link.url}
+                  target="_blank"
+                  aria-label={link.name}
+                  className="text-white text-2xl transition duration-300 transform hover:scale-125"
+                >
+                  <i className={link.icon} />
+                </Link>
+              )
+          )}
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
