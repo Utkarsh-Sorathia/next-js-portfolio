62d7b935a227f94bd93db041280a6d4cc0cefb1a
Utkarsh-Sorathia <83358150+Utkarsh-Sorathia@users.noreply.github.com>
2025-06-18 06:07:17 +0000

    removed old google token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -124,10 +124,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
-        <meta
-          name="google-site-verification"
-          content="0IXFzYG-G9iwXixbQXFVanLLZdN0y2CPTZ-5qZbu-Zc"
-        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
