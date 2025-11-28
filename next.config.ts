f4f50d3449f3ac1e65f20c5094c6be82530fa8b3
Utkarsh Sorathia <83358150+Utkarsh-Sorathia@users.noreply.github.com>
2025-11-28 11:43:30 +0530

    Add redirects to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://utkarshsorathia.in/",
+        permanent: true,
+      },
+      {
+        source: "/blogs",
+        destination: "https://utkarshsorathia.in/blogs",
+        permanent: true,
+      },
+      {
+        source: "/blogs/:slug*",
+        destination: "https://utkarshsorathia.in/blogs/:slug*",
+        permanent: true,
+      },
+      {
+        source: "/cv",
+        destination: "https://utkarshsorathia.in/cv",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        destination: "https://utkarshsorathia.in/:path*",
+        permanent: true,
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
