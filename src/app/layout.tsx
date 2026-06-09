fc5db044dc792c0553503d587cd0e7981c46a927
Utkarsh-Sorathia <utkarshsor03@gmail.com>
2026-06-09 16:10:25 +0530

    refactor: move NavigationPulse project ID to env variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -175,12 +175,14 @@ export default function RootLayout({
               strategy="lazyOnload"
             />
           )}
-          <Script
-            src="https://app.navigationpulse.com/navigationpulse.min.js"
-            data-pid="c2dfa52e-7c76-471a-a2bf-e9cac3966ce7"
-            data-console="true"
-            strategy="afterInteractive"
-          />
+          {process.env.NEXT_PUBLIC_NAVIGATIONPULSE_PID && (
+            <Script
+              src="https://app.navigationpulse.com/navigationpulse.min.js"
+              data-pid={process.env.NEXT_PUBLIC_NAVIGATIONPULSE_PID}
+              data-console="true"
+              strategy="afterInteractive"
+            />
+          )}
           <Header className="app_nav" navItems={navMenus} />
           <main id="main-content" className="flex-grow">{children}</main>
           <Analytics />
